f0961a0df7044920bdd34e9803f03950c0bbd3f6
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2025-04-23 18:24:37 +0300

    add wholesale orders page

--- src/js/modules/inputs-mask.js
+++ src/js/modules/inputs-mask.js
@@ -1,3 +1,5 @@
+import AirDatepicker from "air-datepicker";
+
 export const inputsMask = () => {
   document.querySelectorAll(".input__valid-phone").forEach((el) => {
     IMask(el, {
@@ -7,6 +9,12 @@ export const inputsMask = () => {
   document.querySelectorAll(".input__valid-inn").forEach((el) => {
     IMask(el, {
       mask: "0000000000",
+    });
+  });
+  document.querySelectorAll(".date-picker").forEach((input) => {
+    new AirDatepicker(input);
+    IMask(input, {
+      mask: "00.00.0000",
     });
   });
 };
